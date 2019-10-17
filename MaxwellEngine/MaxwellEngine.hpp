e732863be839f306dd1afbcbebc920d46f398f6a
gtier <30325272+gtier@users.noreply.github.com>
2019-10-17 15:09:23 -0700

    Initial Version Control Commit

--- MaxwellEngine/MaxwellEngine.hpp
+++ MaxwellEngine/MaxwellEngine.hpp
@@ -11,17 +11,21 @@
 
 #include <stdio.h>
 #include <vector>
+#include <map>
 #include <memory>
 #include <utility>
 #include <iostream>
+#include <queue>
 #include "mes.h"
 #include "RenderObject.hpp"
 #include "CameraObject.hpp"
+#include "ShaderProgram.hpp"
 
 class mes::MaxwellEngine
 {
 public:
     typedef std::vector<std::unique_ptr<mes::RenderObject>> VecRenderObject;
+    typedef std::vector<std::unique_ptr<mes::ShaderProgram>> VecShaderProgram;
     typedef void (*renderIntercept_t)(MaxwellEngine& engine);
     typedef void (*handleInput_t)(GLFWwindow* window);
     typedef unsigned long obj_id;
@@ -30,17 +34,24 @@ private:
     GLFWwindow* engineWindow;
     unsigned int engineVertexShader, engineFragmentShader, engineShaderProgram;
     VecRenderObject vecRenderObject;
+    VecShaderProgram vecShaderProgram;
     CameraObject mainCamera;
+    std::vector<handleInput_t> vecHandleInput;
+    bool toHandleInput = false;
+    bool handleKeyConfig = false, handleCursorConfig = false, cursorHandlerMounted = false, keyHandlerMounted = false,
+    cameraIsInputHandler = false;
+    float deltaFrameTime, lastFrameTime, currFrameTime;
+    std::queue<obj_id> emptyQueue;
     
 public:
-    MaxwellEngine(const char* vertShader, const char* fragmentShader)
-    : mainCamera(800, 600)
+    MaxwellEngine(mes::ShaderFile& vertShader, mes::ShaderFile& fragmentShader)
+    : mainCamera(800, 600), deltaFrameTime(0.0f), lastFrameTime(0.0f), currFrameTime(0.0f)
     {
         init(800, 600, vertShader, fragmentShader);
     }
     
-    MaxwellEngine(int width, int height, const char* vertShader, const char* fragmentShader)
-    : mainCamera(width, height)
+    MaxwellEngine(int width, int height, mes::ShaderFile& vertShader, mes::ShaderFile& fragmentShader)
+    : mainCamera(width, height), deltaFrameTime(0.0f), lastFrameTime(0.0f), currFrameTime(0.0f)
     {
         init(width, height, vertShader, fragmentShader);
     }
@@ -52,6 +63,10 @@ public:
     
     template <class T>
     obj_id createRenderObject(mes::VertexDataObject<T>& verts, std::vector<unsigned int>& indices);
+    
+    void deleteRenderObject(const obj_id id);
+    
+    obj_id createShaderProgram(mes::ShaderFile& vertShader, mes::ShaderFile& fragmentShader);
     
     mes::RenderObject& getRenderObject(obj_id id)
     {
@@ -73,8 +88,19 @@ public:
     
     void stopRenderLoop();
     
+    void addInputHandler(const handleInput_t& handler);
+    
+    static void cursorHandler(GLFWwindow* window, double x, double y);
+    
+    //TODO add enum options for different camera modes or objects that control cursor.
+    void mountStaticCamera();
+    
+    void enableCursorHanler();
+    
+    void enableKeyHandler();
+    
 private:
-    void init(int width, int height, const char* vertShader, const char* fragmentShader);
+    void init(int width, int height, mes::ShaderFile& vertShader, mes::ShaderFile& fragmentShader);
     
     void renderLoop(handleInput_t handleInput, renderIntercept_t renderIntercept);
     
